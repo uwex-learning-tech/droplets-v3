4870faf8c0d1fb450818bceea8425d5103095e72
Ethan Lin <ethan.lin@uwex.edu>
2014-01-06 17:28:45 -0600

    Remove Scroll to top and fixed internal page nav
    Scroll top feature is useless in D2L 10.1

--- src/script/style.js
+++ src/script/style.js
@@ -249,8 +249,8 @@ $(document).ready(function() {
 		nav += "</ul>";
 		
 		$(".with-subnav").prepend(nav);
-		$("body").css("padding-top",$(".page-subnav").innerHeight());
-		$(".anchor").css({"display":"block", "position":"relative", "top":($(".page-subnav").innerHeight() * -1)+"px", "visibility":"hidden"});
+		/* $("body").css("padding-top",$(".page-subnav").innerHeight()); */
+		$(".anchor").css({"display":"block", "position":"relative", "top":"-32px", "visibility":"hidden"});
 		
 	} // end getSubnav
 	
@@ -258,6 +258,8 @@ $(document).ready(function() {
 	checkComponents();
 	
 });
+
+/* jshint ignore:start */
 
 function getParameterByName(url,name) {
 	var regexS = "[\\?&]" + name + "=([^&#]*)";
@@ -293,21 +295,4 @@ function gotoDiscussions() {
 	var tocURL = 'd2l/le/'+getParameterByName(window.location.href,'ou')+'/discussions/List';
 	top.location.href = tocURL;
 }
-
-/*********************************************
-SCROLL TO TOP JS
-*********************************************/
-/*
-
- scrollup v2.1.0
- Author: Mark Goodyear - http://markgoodyear.com
- Git: https://github.com/markgoodyear/scrollup
-
- Copyright 2013 Mark Goodyear.
- Licensed under the MIT license
- http://www.opensource.org/licenses/mit-license.php
-
- Twitter: @markgdyr
-
- */
-!function(a,b,c){a.fn.scrollUp=function(b){a.data(c.body,"scrollUp")||(a.data(c.body,"scrollUp",!0),a.fn.scrollUp.init(b))},a.fn.scrollUp.init=function(d){var e=a.fn.scrollUp.settings=a.extend({},a.fn.scrollUp.defaults,d),f=e.scrollTitle?e.scrollTitle:e.scrollText,g=a("<a/>",{id:e.scrollName,href:"#top",title:f}).appendTo("body");e.scrollImg||g.html(e.scrollText),g.css({display:"none",position:"fixed",zIndex:e.zIndex}),e.activeOverlay&&a("<div/>",{id:e.scrollName+"-active"}).css({position:"absolute",top:e.scrollDistance+"px",width:"100%",borderTop:"1px dotted"+e.activeOverlay,zIndex:e.zIndex}).appendTo("body"),scrollEvent=a(b).scroll(function(){switch(scrollDis="top"===e.scrollFrom?e.scrollDistance:a(c).height()-a(b).height()-e.scrollDistance,e.animation){case"fade":a(a(b).scrollTop()>scrollDis?g.fadeIn(e.animationInSpeed):g.fadeOut(e.animationOutSpeed));break;case"slide":a(a(b).scrollTop()>scrollDis?g.slideDown(e.animationInSpeed):g.slideUp(e.animationOutSpeed));break;default:a(a(b).scrollTop()>scrollDis?g.show(0):g.hide(0))}}),g.click(function(b){b.preventDefault(),a("html, body").animate({scrollTop:0},e.topSpeed,e.easingType)})},a.fn.scrollUp.defaults={scrollName:"scrollUp",scrollDistance:300,scrollFrom:"top",scrollSpeed:300,easingType:"linear",animation:"fade",animationInSpeed:200,animationOutSpeed:200,scrollText:"Scroll to top",scrollTitle:!1,scrollImg:!1,activeOverlay:!1,zIndex:2147483647},a.fn.scrollUp.destroy=function(d){a.removeData(c.body,"scrollUp"),a("#"+a.fn.scrollUp.settings.scrollName).remove(),a("#"+a.fn.scrollUp.settings.scrollName+"-active").remove(),a.fn.jquery.split(".")[1]>=7?a(b).off("scroll",d):a(b).unbind("scroll",d)},a.scrollUp=a.fn.scrollUp}(jQuery,window,document);
+/* jshint ignore:end */
