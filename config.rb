1e241c969b072208b8ec8e6d624d87c455637685
Ethan Lin <ethan.lin@uwex.edu>
2014-08-04 16:05:40 -0500

    Cleaning up and updated support page

---
+++ config.rb
@@ -0,0 +1,21 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "guide/src/css/"
+sass_dir = "guide/src/css/"
+images_dir = "guide/src/img/"
+javascripts_dir = "guide/src/script/"
+fonts_dir = "guide/src/fonts/"
+
+output_style = :nested
+environment = :development
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+preferred_syntax = :scss
