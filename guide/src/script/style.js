4c063141e5948f13b01d3ac33f52c2b35606b772
Ethan Lin <ethan.lin@uwex.edu>
2017-11-29 10:34:11 -0600

    Updated version number in code comments

--- guide/src/script/style.js
+++ guide/src/script/style.js
@@ -1,10 +1,10 @@
 /*
  * DROPLET Framework jQuery Core Script
  *
- * @version: 1.5.1
+ * @version: 1.5.2
  * @author: Ethan Lin
  * @url: https://github.com/oel-mediateam/idstyleguide
- * Released date: 11/07/2017
+ * Released date: 11/29/2017
  *
  * @license: The MIT License (MIT)
  * Copyright (c) 2013-2017 UWEX CEOEL Media Services
