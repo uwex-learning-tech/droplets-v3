e44fc59f6e8f150740c8bf4d46953aa620e2086b
Ethan Lin <ethan.lin@uwex.wisconsin.edu>
2023-08-17 14:01:02 -0500

    file restructured

---
+++ webpack.common.js
@@ -0,0 +1,49 @@
+const path = require( 'path' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+
+module.exports = {
+    entry: {
+        'droplets' : [
+            path.resolve(__dirname, './droplets/assets/droplets-dev.js'),
+            path.resolve(__dirname, './droplets/assets/prism.js'),
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve( __dirname, 'dist', 'assets' ),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                          // Prefer `dart-sass`
+                          implementation: require('sass'),
+                        },
+                    },
+                    
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin( {
+            template: 'droplets/index.html',
+            chunksSortMode: 'auto',
+            filename: path.resolve( __dirname, 'dist', 'index.html' ),
+        } ),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        } ),
+    ],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+
+ module.exports = merge( common, {
+   mode: 'production',
+   plugins: [
+    new webpack.BannerPlugin( {
+        banner: '(c) 2018-2023 Learning Technology, University of Wisconsin Extended Campus',
+        entryOnly: true
+    } ),
+  ],
+   optimization: {
+      minimizer: [
+        new TerserPlugin( 
+          { extractComments: false, }
+        )
+      ],
+    },
+} );
